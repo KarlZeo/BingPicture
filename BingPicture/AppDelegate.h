6fb1a3efb393769c0e81498b6bd739e93403603c
ZEO <nieyujiang@gmail.com>
2017-07-11 21:42:28 +0800

    Initial Commit

---
+++ BingPicture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BingPicture
+//
+//  Created by Zeo on 11/07/2017.
+//  Copyright © 2017 Zeo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BingPicture/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BingPicture
+//
+//  Created by Zeo on 11/07/2017.
+//  Copyright © 2017 Zeo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
